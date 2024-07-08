b578121fa09dfd156b638b3ab444437e1d5e4dfc
Aadhya Pandillapally <aadhyamsu@gmail.com>
2024-07-09 03:40:39 +0530

    adjusted image rules in web config

--- webpack.config.js
+++ webpack.config.js
@@ -31,16 +31,9 @@ module.exports = {
      {
       test: /\.(png|svg|jpg|jpeg|gif)$/i,
       type: 'asset/resource',
-      use: [
-        {
-          loader: 'file-loader',
-          options: {
-            name: '[name].[ext]',
-            outputPath: 'images/',
-            publicPath: 'images/'
-          }
-        }
-      ]
+      generator: {
+        filename: 'assets/images/[name][ext][query]',
+      },
     },
     {
       test: /\.(woff|woff2|eot|ttf|otf)$/i,
